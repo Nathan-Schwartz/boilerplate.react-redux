8cc01d4478417ed071387ae4cc7bc2b58fc7bea7
Nathan Schwartz <nathan.schwartz95@gmail.com>
2016-08-16 12:39:46 -0500

    update readme, comment homeContainer

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
+This boilerplate was made to provide a good starting point for React-Redux apps.
+
+It has been commented to make it easy to understand, and instructional for people who are new to this stack.
+
+
+## To install dependencies
+> npm install
 
 ## To start the server
 > npm start
 
+## To run the server (restart on changes)
+> npm run watch
+
 ## To lint the client folder
 > npm run lint
+
+
+If you have any suggestions or questions please feel free to open an issue.
+
+If you would like to contribute, feel free to make a pull request.
--- client/containers/homeContainer.js
+++ client/containers/homeContainer.js
@@ -6,11 +6,23 @@ import Home from '../components/home';
 // We use Redux for all app state.
 // Any component that needs state is considered a container.
 // connect() returns a component that has Redux baked in.
-const mapState = (store) => ({
+
+// The two functions below return objects.
+// The keys in each object are added to the child component specified on line 34
+// (in this case Home) as props. Within Home you could access them as: 
+// this.props.textFieldValue
+// -or-
+// this.props.clearTextFieldValue()
+
+// This function allows you to choose which store properties you want to pass in.
+const mapStateToProps = (store) => ({
   textFieldValue: store.textField
 });
 
-const mapDispatch = (dispatch) => ({
+// This function returns an object with methods
+// Each method will dispatch an action. In this way you can limit
+// which components are aware of Redux.
+const mapDispatchToProps = (dispatch) => ({
   updateTextFieldValue: (newText) => {
     dispatch({ type: UPDATE_TEXT, data: newText });
   },
@@ -19,4 +31,4 @@ const mapDispatch = (dispatch) => ({
   }
 });
 
-export default connect(mapState, mapDispatch)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
